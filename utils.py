368e60d24de4688466a279650de15b553b339fb6
David Novak <31126178+davnovak@users.noreply.github.com>
2024-03-05 11:11:53 +0100

    Make TensorFlow 2 compatible

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import math
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.compat.v1.disable_eager_execution()
+tf.disable_v2_behavior() 
 import numpy as np
 
 def asinh(x, scale=5.):
